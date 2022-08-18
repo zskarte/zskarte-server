c64df72bddbb2631729ee7a192f657c83120aae3
SwitzerChees <patrickmichel96@yahoo.de>
2022-08-18 13:49:23 +0200

    Readd src/admin stuff

---
+++ src/admin/webpack.config.example.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+module.exports = (config, webpack) => {
+  // Note: we provide webpack above so you should not `require` it
+  // Perform customizations to webpack config
+  // Important: return the modified config
+  return config;
+};
