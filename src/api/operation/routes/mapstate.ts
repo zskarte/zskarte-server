bf8aec0f1353a4fcd6dc4d42e95e3252cbcbd33d
swerder <1330346+swerder@users.noreply.github.com>
2024-02-18 17:24:48 +0100

    add granular update functions for operation

--- src/api/operation/routes/mapstate.ts
+++ src/api/operation/routes/mapstate.ts
@@ -5,6 +5,15 @@ export default {
   routes: [
     {
       method: 'POST',
+      path: '/operations/mapstate/currentlocation',
+      handler: 'operation.currentLocation',
+      config: {
+        //this route does not update Operation itself therefore no UPDATE_BY_ID checks are needed
+        middlewares: [CreateAccessControlMiddlewareConfig({type:'api::operation.operation', check: AccessControlTypes.BY_ID})]
+      }
+    },
+    {
+      method: 'POST',
       path: '/operations/mapstate/patch',
       handler: 'operation.patch',
       config: {
